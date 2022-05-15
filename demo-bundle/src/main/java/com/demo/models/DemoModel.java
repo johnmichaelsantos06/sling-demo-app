264b9e03ec318f8fbab55b888a5b814b18d492c8
John Michael Santos <johnmichaelsantos06@gmail.com>
2022-05-15 17:48:54 +0800

    Additional conditional logic

--- demo-bundle/src/main/java/com/demo/models/DemoModel.java
+++ demo-bundle/src/main/java/com/demo/models/DemoModel.java
@@ -1,6 +1,10 @@
 package com.demo.models;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.Self;
@@ -11,7 +15,21 @@ public class DemoModel {
 	@Self
     private SlingHttpServletRequest request;
 	
-	public String getParameters() {
-		return request.getParameter("parameter");
+	public boolean getRefreshButtonDisplayed() {
+		boolean isDisplayed = false;
+		
+		Resource resource = request.getResource();
+		String parentName = resource.getParent().getName();
+		
+		String dateNow = getFormattedDate(new Date());
+		if (parentName.equals(dateNow)) {
+			isDisplayed = true;
+		}
+		
+		return isDisplayed;
+	}
+	
+	private String getFormattedDate(Date date) {
+		return new SimpleDateFormat("yyyyMMdd").format(date);
 	}
 }
